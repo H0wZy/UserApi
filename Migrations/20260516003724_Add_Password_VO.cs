f0c6db834f9a34b673a7b5f8699792759e6e1526
H0wZy <h0wzymarcos@gmail.com>
2026-05-15 23:16:02 -0300

    feat: add new migrations

---
+++ Migrations/20260516003724_Add_Password_VO.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace user_api.cs.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20260516003724_Add_Password_VO : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
