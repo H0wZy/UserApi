267f8e5a21da93d16b3e97bf7564a8e81ccb8e71
H0wZy <h0wzymarcos@gmail.com>
2026-05-20 22:20:25 -0300

    feat: CpfTest

--- tests/UserApi.Tests/CpfTest.cs
+++ tests/UserApi.Tests/CpfTest.cs
@@ -1,10 +1,27 @@
-﻿namespace UserApi.Tests;
+﻿using UserApi.ValueObjects;
+
+namespace UserApi.Tests;
 
 public class CpfTest
 {
-    [Fact]
-    public void Test1()
+    [Fact(DisplayName = "Create with all same digits should return failure.")]
+    public void Create_WithAllSameDigits_ShouldReturnFailure()
     {
+        var cpf = "00000000000";
 
+        var result = Cpf.Create(cpf);
+
+        Assert.True(result.IsFailure);
+    }
+
+    // TODO FIX THIS:
+    [Fact(DisplayName = "Create with points should return failure.")]
+    public void Create_WithPoints_ShouldReturnSuccess()
+    {
+        var cpf = "801.639.350-09";
+
+        var result = Cpf.Create(cpf);
+
+        Assert.True(result.IsFailure);
     }
 }
